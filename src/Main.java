bbbdb7c2d667a01da8e06f24833ae7f83ce70cec
Роман <romanze1706@gmail.com>
2022-11-02 16:25:40 +0300

    Проверьте задание 8

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,13 @@ var difference = secondBoxerWeight - firstBoxerWeight;
 System.out.println("Разница в весе двух боксёров равна " + difference + "кг");
 var percentDifference = totalWeightBoxers % firstBoxerWeight;
 System.out.println("Разница в весе в процентном соотношении равна " + percentDifference);
+var workingHours = 640;
+var timeWorker = 8;
+var allWorkers = workingHours / timeWorker;
+System.out.println("Всего работников в компании – " + allWorkers + " человек");
+allWorkers = allWorkers + 94;
+timeWorker = workingHours / allWorkers;
+System.out.println("Если в компании работает " + allWorkers + " человека, то всего " + timeWorker +
+        " часа работы может быть поделено между сотрудниками");
     }
 }
