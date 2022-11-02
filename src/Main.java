80aa44ed600c8de9590ec5833d0fbee6d2fa25f6
Роман <romanze1706@gmail.com>
2022-11-02 15:47:00 +0300

    Проверьте задание 6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+var firstBoxerWeight = 78.2;
+var secondBoxerWeight = 82.7;
+var totalWeightBoxers = firstBoxerWeight + secondBoxerWeight;
+System.out.println("общий вес двух боксёров" + totalWeightBoxers + "кг");
+var difference = secondBoxerWeight - firstBoxerWeight;
+System.out.println("Разница в весе двух боксёров равна" + difference + "кг");
     }
 }
