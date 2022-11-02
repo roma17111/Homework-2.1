bcbb50c3292736d8776c8675aaa037fa7615a0da
Роман <romanze1706@gmail.com>
2022-11-02 16:02:31 +0300

    Проверьте задание 7

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ public class Main {
 var firstBoxerWeight = 78.2;
 var secondBoxerWeight = 82.7;
 var totalWeightBoxers = firstBoxerWeight + secondBoxerWeight;
-System.out.println("общий вес двух боксёров" + totalWeightBoxers + "кг");
+System.out.println("общий вес двух боксёров " + totalWeightBoxers + "кг");
 var difference = secondBoxerWeight - firstBoxerWeight;
-System.out.println("Разница в весе двух боксёров равна" + difference + "кг");
+System.out.println("Разница в весе двух боксёров равна " + difference + "кг");
+var percentDifference = totalWeightBoxers % firstBoxerWeight;
+System.out.println("Разница в весе в процентном соотношении равна " + percentDifference);
     }
 }
